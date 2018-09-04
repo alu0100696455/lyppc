4441aa0f71efbf0bf7365f7fd5c0bc584fe8e509
Jonathan <alu0100696455@ull.edu.es>
2018-09-04 13:58:50 +0100

    Más resolutores

--- alimentacion/urls.py
+++ alimentacion/urls.py
@@ -4,10 +4,43 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
     path('stigler/', views.stigler, name='stigler'),
-    path('stigler/muestreo_<int:muestreo>/', views.stigler, name='stigler'),
-    path('stigler/muestreo_<int:muestreo>/repeticiones_<int:repeticiones>', views.stigler, name='stigler'),
-    path('knapsack_multidimensional', views.knapsack_multidimensional, name='knapsack_multidimensional'),
-    path('knapsack_multidimensional/muestreo_<int:muestreo>/', views.knapsack_multidimensional, name='knapsack_multidimensional'),
-    path('knapsack_multidimensional/muestreo_<int:muestreo>/repeticiones_<int:repeticiones>', views.knapsack_multidimensional, name='knapsack_multidimensional'),
+    path('stigler/muestreo-<int:muestreo>/', views.stigler, name='stigler'),
+    path('stigler/repeticiones-<int:repeticiones>', views.stigler, name='stigler'),
+    path('stigler/muestreo-<int:muestreo>/repeticiones-<int:repeticiones>', views.stigler, name='stigler'),
+    path('stigler/test-mode/', views.stigler, {'test_mode': True}),
+
+    path('stigler-cbc/', views.stigler_cbc, name='stigler_cbc'),
+    path('stigler-cbc/muestreo-<int:muestreo>/', views.stigler_cbc, name='stigler_cbc'),
+    path('stigler-cbc/repeticiones-<int:repeticiones>', views.stigler_cbc, name='stigler_cbc'),
+    path('stigler-cbc/muestreo-<int:muestreo>/repeticiones-<int:repeticiones>', views.stigler_cbc, name='stigler_cbc'),
+    path('stigler-cbc/test-mode/', views.stigler_cbc, {'test_mode': True}),
+
+    path('stigler-glop/', views.stigler_glop, name='stigler_glop'),
+    path('stigler-glop/muestreo-<int:muestreo>/', views.stigler_glop, name='stigler_glop'),
+    path('stigler-glop/repeticiones-<int:repeticiones>', views.stigler_glop, name='stigler_glop'),
+    path('stigler-glop/muestreo-<int:muestreo>/repeticiones-<int:repeticiones>', views.stigler_glop, name='stigler_glop'),
+    path('stigler-glop/test-mode/', views.stigler_glop, {'test_mode': True}),
+
+    path('knapsack-multidimensional/', views.knapsack_multidimensional, name='knapsack_multidimensional'),
+    path('knapsack-multidimensional/muestreo-<int:muestreo>/', views.knapsack_multidimensional, name='knapsack_multidimensional'),
+    path('knapsack-multidimensional/repeticiones-<int:repeticiones>', views.knapsack_multidimensional, name='knapsack_multidimensional'),
+    path('knapsack-multidimensional/muestreo-<int:muestreo>/repeticiones-<int:repeticiones>', views.knapsack_multidimensional, name='knapsack_multidimensional'),
+    path('knapsack-multidimensional/test-mode/', views.knapsack_multidimensional, {'test_mode': True}),
+
+    path('knapsack-multidimensional-cbc/', views.knapsack_multidimensional_cbc, name='knapsack_multidimensional_cbc'),
+    path('knapsack-multidimensional-cbc/muestreo-<int:muestreo>/', views.knapsack_multidimensional_cbc, name='knapsack_multidimensional_cbc'),
+    path('knapsack-multidimensional-cbc/repeticiones-<int:repeticiones>', views.knapsack_multidimensional_cbc, name='knapsack_multidimensional_cbc'),
+    path('knapsack-multidimensional-cbc/muestreo-<int:muestreo>/repeticiones-<int:repeticiones>', views.knapsack_multidimensional_cbc, name='knapsack_multidimensional_cbc'),
+    path('knapsack-multidimensional-cbc/test-mode/', views.knapsack_multidimensional_cbc, {'test_mode': True}),
+
+    path('knapsack-multidimensional-bandb/', views.knapsack_multidimensional_bandb, name='knapsack_multidimensional_bandb'),
+    path('knapsack-multidimensional-bandb/muestreo-<int:muestreo>/', views.knapsack_multidimensional_bandb, name='knapsack_multidimensional_bandb'),
+    path('knapsack-multidimensional-bandb/repeticiones-<int:repeticiones>', views.knapsack_multidimensional_bandb, name='knapsack_multidimensional_bandb'),
+    path('knapsack-multidimensional-bandb/muestreo-<int:muestreo>/repeticiones-<int:repeticiones>', views.knapsack_multidimensional_bandb, name='knapsack_multidimensional_bandb'),
+    path('knapsack-multidimensional-bandb/test-mode/', views.knapsack_multidimensional_bandb, {'test_mode': True}),
+
+    path('data-for-tests/', views.data_for_tests, name='data_for_tests'),
+    path('data-for-tests/muestreo-<int:muestreo>/', views.data_for_tests, name='data_for_tests'),
 ]
--- alimentacion/views.py
+++ alimentacion/views.py
@@ -3,6 +3,7 @@ from django.db.models import Max
 
 import random
 import atexit
+import copy
 from time import time
 from datetime import timedelta
 
@@ -16,38 +17,55 @@ from ortools.algorithms import pywrapknapsack_solver
 def index(request):
     return render(request, 'alimentacion/index.html')
 
+class Datos:
+    data_for_tests = []
+    # [nutriente, mínimo recomendado, máximo recomendado]
+    DEFAULTS_NUTRIENTS = [
+        ['Calorías (1000s)', 2, 4],
+        ['Proteínas (g)', 50, 200],
+        ['Calcio (g)', 1, 2.5],
+        ['Hierro (mg)', 18, 45],
+        ['Vitamina A (1000 IU)', 3, 10],
+        ['Vitamina B1 (mg)', 1.2, 4],
+        ['Vitamina B2 (mg)', 1.3, 4],
+        ['Vitamina B3 (mg)', 16, 35],
+        ['Vitamina C (mg)', 90, 2000]]
 
-def stigler(request, muestreo=0, repeticiones=1):
+
+# Stigler diet
+
+def stigler_cbc(request, test_mode=False, muestreo=0, repeticiones=1):
+    solver = pywraplp.Solver('SolveStigler', pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
+    return stigler(request, solver, test_mode, muestreo, repeticiones)
+
+def stigler_glop(request, test_mode=False, muestreo=0, repeticiones=1):
+    solver = pywraplp.Solver('SolveStigler', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+    return stigler(request, solver, test_mode, muestreo, repeticiones)
+
+def stigler(request, solver=None, test_mode=False, muestreo=0, repeticiones=1):
     # Nombre, Unidad, Precio (euros), Calorias, Proteínas (g), Calcio (g), Hierro (mg),
     # Vitamina A (IU), Vitamina B1 (mg), Vitamina B2 (mg), Vitamina B3 (mg), Vitamina C (mg)
-    if muestreo == 0:
-        data = list(Alimento.objects.values_list('nombre', 'unidad', 'precio', 'calorias', 'proteinas', 'calcio', 'hierro', 'vitamina_a', 'vitamina_b1', 'vitamina_b2', 'vitamina_b3', 'vitamina_c'))
+    if test_mode is False:
+        if muestreo == 0:
+            data = list(Alimento.objects.values_list('nombre', 'unidad', 'precio', 'calorias', 'proteinas', 'calcio', 'hierro', 'vitamina_a', 'vitamina_b1', 'vitamina_b2', 'vitamina_b3', 'vitamina_c'))
+        else:
+            data = []
+            for i in range(muestreo):
+                # nombre = aleatorio, unidad = 1~500,, precio = 0.01~1.00
+                # calorias = 1~50, protenias = 0~2000, calcio = 0~20
+                # hierro = 0~500, vitamina A = 0~200, vitamina B1 = 0~50
+                # vitamina B2 = 0~100, vitamina B3 = 0~500 , vitamina C = 0~3000
+                data += [[
+                    'Alimento ' + str(i + 1),       random.uniform(1, 100),         random.uniform(1.00, 10.00),
+                    random.uniform(0.00, 2.00),     random.uniform(0.00, 50.00),    random.uniform(0.00, 1.30),
+                    random.uniform(0.00, 8.00),     random.uniform(0.00, 3.00),     random.uniform(0.00, 1.20),
+                    random.uniform(0.00, 1.30),     random.uniform(0.00, 16.00),    random.uniform(0.00, 90.00)
+                ]]
     else:
-        data = []
-        for i in range(muestreo):
-            # nombre = aleatorio, unidad = 1~500,, precio = 0.01~1.00
-            # calorias = 1~50, protenias = 0~2000, calcio = 0~20
-            # hierro = 0~500, vitamina A = 0~200, vitamina B1 = 0~50
-            # vitamina B2 = 0~100, vitamina B3 = 0~500 , vitamina C = 0~3000
-            data += [[
-                'Alimento ' + str(i + 1),       random.uniform(1, 500),         random.uniform(0.01, 1.00),
-                random.uniform(1.00, 50.00),    random.uniform(0.00, 2000.00),  random.uniform(0.00, 20.00),
-                random.uniform(0.00, 500.00),   random.uniform(0.00, 200.00),   random.uniform(0.00, 50.00),
-                random.uniform(0.00, 100.00),   random.uniform(0.00, 500.00),   random.uniform(0.00, 3000.00)
-            ]]
+        data = Datos.data_for_tests
 
     # Nutrient minimums.
-    nutrients = [
-    	['Calorías (1000s)', 2],
-    	['Proteínas (g)', 50],
-    	['Calcio (g)', 1.3],
-    	['Hierro (mg)', 8],
-    	['Vitamina A (1000 IU)', 3],
-    	['Vitamina B1 (mg)', 1.2],
-    	['Vitamina B2 (mg)', 1.3],
-    	['Vitamina B3 (mg)', 16],
-    	['Vitamina C (mg)', 90]]
-
+    nutrients = Datos.DEFAULTS_NUTRIENTS
     nutrient_minimus = nutrients
 
     columns_names = ['Nombre', 'Unidad (g)', 'Precio (€)'] + [nutrient[0] for nutrient in nutrients]
@@ -59,9 +77,10 @@ def stigler(request, muestreo=0, repeticiones=1):
         #Timer start
         timer_start = time()
 
-
         # Instanciar a Glop solver, SolveStigler.
-        solver = pywraplp.Solver('SolveStigler', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+        if solver is None:
+            solver = pywraplp.Solver('SolveStigler', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+
         # Array para los datos nutricionales.
         food = [[]] * len(data)
 
@@ -139,36 +158,42 @@ def stigler(request, muestreo=0, repeticiones=1):
             return render(request, 'alimentacion/stigler.html', context)
 
 
-def knapsack_multidimensional(request, muestreo=0, repeticiones=1):
+# Knapsack Multidimensional
+
+def knapsack_multidimensional_cbc(request, test_mode=False, muestreo=0, repeticiones=1):
+    solver = pywrapknapsack_solver.KnapsackSolver(pywrapknapsack_solver.KnapsackSolver.KNAPSACK_MULTIDIMENSION_CBC_MIP_SOLVER,
+    "Multi-dimensional solver");
+    return knapsack_multidimensional(request, solver, test_mode, muestreo, repeticiones)
+
+def knapsack_multidimensional_bandb(request, test_mode=False, muestreo=0, repeticiones=1):
+    solver = pywrapknapsack_solver.KnapsackSolver(pywrapknapsack_solver.KnapsackSolver.KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER,
+    "Multi-dimensional solver");
+    return knapsack_multidimensional(request, solver, test_mode, muestreo, repeticiones)
+
+def knapsack_multidimensional(request, solver=None, test_mode=False, muestreo=0, repeticiones=1):
     # Nombre, Unidad, Precio (euros), Calorias, Proteínas (g), Calcio (g), Hierro (mg),
     # Vitamina A (IU), Vitamina B1 (mg), Vitamina B2 (mg), Vitamina B3 (mg), Vitamina C (mg)
-    if muestreo == 0:
-        data = list(Alimento.objects.values_list('nombre', 'unidad', 'precio', 'calorias', 'proteinas', 'calcio', 'hierro', 'vitamina_a', 'vitamina_b1', 'vitamina_b2', 'vitamina_b3', 'vitamina_c'))
+    if test_mode is False:
+        if muestreo == 0:
+            data = list(Alimento.objects.values_list('nombre', 'unidad', 'precio', 'calorias', 'proteinas', 'calcio', 'hierro', 'vitamina_a', 'vitamina_b1', 'vitamina_b2', 'vitamina_b3', 'vitamina_c'))
+        else:
+            data = []
+            for i in range(muestreo):
+                # nombre = aleatorio, unidad = 1~500, precio = 1~100
+                # calorias = 0~2, protenias = 0~50, calcio = 0~1.3
+                # hierro = 0~8, vitamina A = 0~3, vitamina B1 = 0~1.2
+                # vitamina B2 = 0~1.3, vitamina B3 = 0-16 , vitamina C = 0~90
+                data += [[
+                    'Alimento ' + str(i + 1),       random.uniform(1, 100),         random.uniform(1.00, 10.00),
+                    random.uniform(0.00, 2.00),     random.uniform(0.00, 50.00),    random.uniform(0.00, 1.30),
+                    random.uniform(0.00, 8.00),     random.uniform(0.00, 3.00),     random.uniform(0.00, 1.20),
+                    random.uniform(0.00, 1.30),     random.uniform(0.00, 16.00),    random.uniform(0.00, 90.00)
+                ]]
     else:
-        data = []
-        for i in range(muestreo):
-            # nombre = aleatorio, unidad = 1~500, precio = 1~100
-            # calorias = 0~2, protenias = 0~50, calcio = 0~1.3
-            # hierro = 0~8, vitamina A = 0~3, vitamina B1 = 0~1.2
-            # vitamina B2 = 0~1.3, vitamina B3 = 0-16 , vitamina C = 0~90
-            data += [[
-                'Alimento ' + str(i + 1),       random.uniform(1, 500),         random.uniform(1.00, 10.00),
-                random.uniform(0.00, 2.00),     random.uniform(0.00, 50.00),    random.uniform(0.00, 1.30),
-                random.uniform(0.00, 8.00),     random.uniform(0.00, 3.00),     random.uniform(0.00, 1.20),
-                random.uniform(0.00, 1.30),     random.uniform(0.00, 16.00),    random.uniform(0.00, 90.00)
-            ]]
+        data = Datos.data_for_tests
 
     # Nutrient minimums.
-    nutrients = [
-    	['Calorías (1000s)', 2],
-    	['Proteínas (g)', 50],
-    	['Calcio (g)', 1.3],
-    	['Hierro (mg)', 8],
-    	['Vitamina A (1000 IU)', 3],
-    	['Vitamina B1 (mg)', 1.2],
-    	['Vitamina B2 (mg)', 1.3],
-    	['Vitamina B3 (mg)', 16],
-    	['Vitamina C (mg)', 90]]
+    nutrients = Datos.DEFAULTS_NUTRIENTS
 
     nutrient_minimus = nutrients
 
@@ -196,10 +221,11 @@ def knapsack_multidimensional(request, muestreo=0, repeticiones=1):
                 , [float(item[11]) for item in data]]
         capacities = [float(item[1]) for item in nutrients]
 
-        solver = pywrapknapsack_solver.KnapsackSolver(pywrapknapsack_solver.KnapsackSolver.KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER,
-        "Multi-dimensional solver");
+        if solver is None:
+            solver = pywrapknapsack_solver.KnapsackSolver(pywrapknapsack_solver.KnapsackSolver.KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER,
+            "Multi-dimensional solver")
 
-        solver.Init(profits, weights, capacities);
+        solver.Init(profits, weights, capacities)
 
         computed_value = solver.Solve()
 
@@ -224,3 +250,31 @@ def knapsack_multidimensional(request, muestreo=0, repeticiones=1):
     }
 
     return render(request, 'alimentacion/knapsack_multidimensional.html', context)
+
+
+def data_for_tests(request, muestreo=0):
+    if muestreo > 0:
+        Datos.data_for_tests = []
+        for i in range(muestreo):
+            # nombre = aleatorio, unidad = 1~500, precio = 1~100
+            # calorias = 0~2, protenias = 0~50, calcio = 0~1.3
+            # hierro = 0~8, vitamina A = 0~3, vitamina B1 = 0~1.2
+            # vitamina B2 = 0~1.3, vitamina B3 = 0-16 , vitamina C = 0~90
+            Datos.data_for_tests += [[
+                'Alimento ' + str(i + 1),       random.uniform(1, 500),         random.uniform(1.00, 10.00),
+                random.uniform(0.00, 2.00),     random.uniform(0.00, 50.00),    random.uniform(0.00, 1.30),
+                random.uniform(0.00, 8.00),     random.uniform(0.00, 3.00),     random.uniform(0.00, 1.20),
+                random.uniform(0.00, 1.30),     random.uniform(0.00, 16.00),    random.uniform(0.00, 90.00)
+            ]]
+
+    nutrients = Datos.DEFAULTS_NUTRIENTS
+
+    nutrient_minimus = nutrients
+    columns_names = ['Nombre', 'Unidad', 'Precio (€)'] + [nutrient[0] for nutrient in nutrients]
+
+    context = {
+        'foods': Datos.data_for_tests,
+        'columns_names': columns_names,
+        'nutrient_minimus': nutrient_minimus,
+    }
+    return render(request, 'alimentacion/data_for_tests.html', context)
